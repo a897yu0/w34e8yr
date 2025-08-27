57ad70b4f9e397363f855604ca34054bf73c7266
welcomehyunseo <welcomehyunseo>
2025-08-27 13:14:53 +0000

    save

--- src/App.tsx
+++ src/App.tsx
@@ -383,7 +383,7 @@ function App() {
 
       <footer className="w-full min-w-xs max-w-3xl bg-white rounded-lg shadow-sm m-0 dark:bg-gray-800">
         <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://w34.com/" className="hover:underline">W34E8YRz</a>. All Rights Reserved.
+          <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://w34.com/" className="hover:underline">W34E8YR</a>. All Rights Reserved.
           </span>
           <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
             <li>
