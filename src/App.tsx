05ced4f1745ee1dca83f24ee967def09d7c771e9
welcomehyunseo <welcomehyunseo>
2025-09-16 04:51:22 +0000

    save

--- src/App.tsx
+++ src/App.tsx
@@ -163,9 +163,12 @@ function Header(props: HeaderProps): React.JSX.Element {
 
   return (
     <header className="w-full flex flex-row justify-between items-center bg-white p-1 border-b-1 border-black">
-      <a href="/" className="cursor-pointer hover:opacity-80 transition-opacity duration-200">
-        <h1 className="text-2xl font-bold text-black">W34</h1>
-      </a>
+      <div className="flex flex-row justify-center items-center cursor-pointer">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-7 mt-1">
+          <path strokeLinecap="round" strokeLinejoin="round" d="m21 7.5-9-5.25L3 7.5m18 0-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9" />
+        </svg>
+        <h1 className="text-2xl font-bold text-black ml-2">W34</h1>
+      </div>
       <div className="block md:hidden cursor-pointer" onClick={() => toggleSidebarShown()}>
         <div className="w-6 h-6">
           {(isSidebarShown === false) ? (
