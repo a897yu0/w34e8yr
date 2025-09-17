ddfa3431901d15384470399e644591f0d2ae3e76
welcomehyunseo <welcomehyunseo>
2025-09-17 04:48:24 +0000

    save

--- src/App.tsx
+++ src/App.tsx
@@ -339,7 +339,7 @@ function SidebarItem(props: SidebarItemProps): React.JSX.Element {
         }
       }}
       >
-        <div className="shrink-0 w-5 h-5 text-black transition duration-75 group-hover:text-gray-800">
+        <div className="shrink-0 w-6 h-6 text-black transition duration-75 group-hover:text-gray-800">
           {icon}
         </div>
         <span className="flex-1 ms-3 text-black whitespace-nowrap">{text}</span>
@@ -389,7 +389,7 @@ function SidebarDropdownItem(props: SidebarDropdownItemProps): React.JSX.Element
   return (
     <li>
       <button type="button" className="flex items-center w-full p-1 text-base text-black transition duration-75 group hover:bg-gray-100 cursor-pointer" onClick={() => toggleDropdownMenu(id)}>
-        <div className="shrink-0 w-5 h-5 text-black transition duration-75 group-hover:text-gray-800">
+        <div className="shrink-0 w-6 h-6 flex justify-center items-center text-black transition duration-75 group-hover:text-gray-800">
           {icon}
         </div>
         <span className="flex-1 ms-3 text-left rtl:text-right whitespace-nowrap">{text}</span>
@@ -548,6 +548,15 @@ function Sidebar(props: SidebarProps): React.JSX.Element {
             />
           </SidebarDropdownItem>
         </SidebarDropdownItem>
+        <SidebarItem
+          name="versions"
+          icon={
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-full">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12c0 1.268-.63 2.39-1.593 3.068a3.745 3.745 0 0 1-1.043 3.296 3.745 3.745 0 0 1-3.296 1.043A3.745 3.745 0 0 1 12 21c-1.268 0-2.39-.63-3.068-1.593a3.746 3.746 0 0 1-3.296-1.043 3.745 3.745 0 0 1-1.043-3.296A3.745 3.745 0 0 1 3 12c0-1.268.63-2.39 1.593-3.068a3.745 3.745 0 0 1 1.043-3.296 3.746 3.746 0 0 1 3.296-1.043A3.746 3.746 0 0 1 12 3c1.268 0 2.39.63 3.068 1.593a3.746 3.746 0 0 1 3.296 1.043 3.746 3.746 0 0 1 1.043 3.296A3.745 3.745 0 0 1 21 12Z" />
+            </svg>
+          }
+          text="Versions"
+        />
         <div className="mx-1 my-4 border-black border-t-1" />
         <SidebarItem
           name="dashboard"
@@ -1080,7 +1089,7 @@ function ServersManagementPanel(): React.JSX.Element {
 function RegisteredServerPanel(): React.JSX.Element {
   /**
    * Features:
-   * List of accounts logged in with when logged in, where, which platform, email, etc...
+   * List of accounts logged in with when logged in, where, which platform, email, ip, location, android or ios, chrome/safari etc...
    */
 
   return (
