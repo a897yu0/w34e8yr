1d35ef9cc0b52eb271294e4c481b4f2be56078ae
welcomehyunseo <welcomehyunseo>
2025-09-19 19:46:51 +0000

    save

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,25 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    react(), 
-    tailwindcss(), 
+    react(),
+    tailwindcss(),
   ],
+  build: {
+    target: 'esnext', // Target modern JavaScript only
+    sourcemap: false, // Disable sourcemaps in production
+    minify: 'esbuild', // Enable esbuild for minification
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+  },
+  esbuild: {
+    drop: ['console', 'debugger'],
+  },
   base: "/w34e8yr",
-})
+});
