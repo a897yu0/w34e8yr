c20f750f595cc3125f6d57d780d8c9eedf1949d8
welcomehyunseo <welcomehyunseo>
2025-09-22 04:35:32 +0000

    save

--- src/components/pages/FallbackPage.tsx
+++ src/components/pages/FallbackPage.tsx
@@ -4,7 +4,7 @@ function FallbackPage(props: FallbackPageProps): React.JSX.Element {
   props;
 
   return (
-    <div className="w-full h-full flex justify-center items-center">
+    <div className="w-full h-full flex justify-center items-center bg-transparent">
       {/* Animated dots */}
       <div className="flex space-x-1">
         <div className="w-2 h-2 bg-blue-500 rounded-full animate-bounce" style={{ animationDelay: '0ms' }}></div>
